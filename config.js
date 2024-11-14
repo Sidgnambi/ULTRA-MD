3f0061278af419088e577a614cbf88ae3107262a
Sidgnambi <131413445+Sidgnambi@users.noreply.github.com>
2024-11-14 16:34:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS = 'your number';  // put your number here
+const ownervb = process.env.OWNERS = '237675123675';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.pairingNumber = process.env.BOT_NUMBER = 'your number';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID = 'session id';  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER = '237675123675';  // put your number here
+global.SESSION_ID = process.env.SESSION_ID = 'GlobalTechInfo~eAJymT4I#9S4igRXiNfldpx5jCbmhwr2K12o4DZrevcLcTdIO6Us';  // put your session id here
 
 global.mods = []
 global.prems = []
